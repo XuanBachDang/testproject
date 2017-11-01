e6916878cf99f1302b91672a554101ab87573d77
BachDang <xuanbach96@gmail.com>
2017-11-02 03:57:35 +0700

    up lrn
    up len

---
+++ MvcMusicStore/MvcMusicStore/App_Start/RouteConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcMusicStore
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "id",
+                url: "category/{store}/{id}",
+                defaults: new { Controller = "Store", action = "Details", id = "" });
+
+            routes.MapRoute(
+                name: "default",
+                url: "{Controller}/{action}/{id}",
+                defaults: new { Controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Browse",
+                url: " Store/{browse}/{Genre}",
+                defaults: new { Controller = "Sotre", action = "Browse", genre = "" });
+            routes.MapRoute(
+                name: "Product list of deafault",
+                url: "category/store",
+                defaults: new {Controller="StoreMangager", Action="Details"}
+                );
+
+        }
+    }
+}
---
+++ MvcMusicStore/MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        public ActionResult Index()
+        {
+            var albums = GetTopSellingAlbums(5);
+            return View(albums);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        // hàm đém số lần mua album
+        private List<Album> GetTopSellingAlbums (int count)
+        {
+            // Group the order details by album and return
+            // the Albums with the highest count
+            // return giá trị count. Sắp xếp giảm dần và get giá trị count 
+            return storeDB.Albums.OrderByDescending(a => a.OrderDetails.Count())
+                .Take(count)
+                .ToList();
+        }
+    }
+}
